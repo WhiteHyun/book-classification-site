edd840af9138a18b0005b66ea27b7529bd460000
WhiteHyun <cx_nf@naver.com>
2020-09-03 15:58:54 +0900

    JS: Books & Home OK Button

--- main.js
+++ main.js
@@ -68,3 +68,22 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+
+// Books control
+const books = document.querySelector(".ok__button");
+books.addEventListener("click", (event) => {
+  const books = document.getElementById("books");
+  const random_number = Math.floor(Math.random() * 3 + 1);
+  const picture = "imgs/search/book" + random_number + ".png";
+  const books_position = document.getElementsByClassName("books__position")[0];
+  const books_img = document.getElementsByClassName("books__img")[0];
+  const books_description = document.getElementsByClassName("books__description")[0];
+
+  if (books_img !== undefined || books_description !== undefined) {
+    books_position.removeChild(books_img);
+    books_position.removeChild(books_description);
+  }
+  books.style.display = 'block';
+  books_position.innerHTML += '<img src="' + picture + '" class="books__img" width="450" /><h3 class="books__description">위치(층): 3층 <br /> 번호: 가C1820 <br /> 발행년도: 2015.02.21 <br /> 저자: 책찾는여우</h3> ';
+});
